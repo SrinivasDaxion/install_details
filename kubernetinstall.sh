3679789334660f83b7f3fd93206260ea9bc6d990
Sriniavsd <96572502+SrinivasDaxion@users.noreply.github.com>
2023-06-16 12:06:53 +0530

    Update kubernetinstall.sh

--- kubernetinstall.sh
+++ kubernetinstall.sh
@@ -40,6 +40,7 @@ mkdir -p /etc/containerd
 containerd config default | tee /etc/containerd/config.toml   <<<<<<<<<<< manually edit and change systemdCgroup to true
 systemctl restart containerd
 
+#Disable SWAP
 swapoff -a  <<<<<<<< just disable it in /etc/fstab instead
 
 apt-get update
